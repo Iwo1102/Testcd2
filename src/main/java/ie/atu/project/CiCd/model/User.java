135e70a2dd045908d9994be640b4315d518a3f6c
G00398070 <G00398070@atu.ie>
2022-11-11 16:23:25 +0000

    created user class

---
+++ src/main/java/ie/atu/project/CiCd/model/User.java
@@ -0,0 +1,15 @@
+package ie.atu.project.CiCd.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "User")
+
+public class User {
+}
