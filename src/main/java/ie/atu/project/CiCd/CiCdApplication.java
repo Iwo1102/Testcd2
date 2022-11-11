955fa9701d3be4b7d299483b1614e06f3a5906f8
g00390383 <g00390383@atu.ie>
2022-11-11 15:40:20 +0000

    Created project

---
+++ src/main/java/ie/atu/project/CiCd/CiCdApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.project.CiCd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiCdApplication.class, args);
+	}
+
+}
