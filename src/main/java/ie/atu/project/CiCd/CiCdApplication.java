1fdd24c278536f2cd2c080f2d50917333d8ed0d4
Iwo <g00391231@atu.ie>
2022-12-04 17:02:06 +0000

    Added test print

--- src/main/java/ie/atu/project/CiCd/CiCdApplication.java
+++ src/main/java/ie/atu/project/CiCd/CiCdApplication.java
@@ -11,7 +11,7 @@ public class CiCdApplication {
 	public static void main(String[] args) {
 		System.out.println("hello world\n");
 		SpringApplication.run(CiCdApplication.class, args);
-
+		System.out.println("Test\n");
 
 	}
 
--- src/main/java/ie/atu/project/CiCd/model/Webpage.java
+++
@@ -1,9 +0,0 @@
-package ie.atu.project.CiCd.model;
-
-import org.springframework.stereotype.Service;
-
-@Service
-
-public class Webpage {
-
-}
