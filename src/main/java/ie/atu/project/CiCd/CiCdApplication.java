af1050b9be1b88025fc5ab4d4339179a18760c6f
Iwo <g00391231@atu.ie>
2022-12-04 16:09:04 +0000

    webpage added

--- src/main/java/ie/atu/project/CiCd/CiCdApplication.java
+++ src/main/java/ie/atu/project/CiCd/CiCdApplication.java
@@ -2,7 +2,9 @@ package ie.atu.project.CiCd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CiCdApplication {
 
---
+++ src/main/java/ie/atu/project/CiCd/model/Webpage.java
@@ -0,0 +1,8 @@
+package ie.atu.project.CiCd.model;
+
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class Webpage {
+}
